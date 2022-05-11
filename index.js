fa645ad069f7123bf905702d7214ddc41b933c0c
Alex <alex@passiolife.com>
2022-05-11 15:51:51 -0400

    Rework Lookups

--- index.js
+++ index.js
@@ -1,24 +1,60 @@
 const { Toolkit } = require("actions-toolkit");
 // g1 is the keyword | g2 is issue number without #
 const ISSUE_KW = /(?:^|(?<= |\t|,|\.|;|"|'|`))(close|closes|closed|fixed|fix|fixes|resolve|resolves|resolved)\s+#(\d+)/gim
+const PRID = /(?:^|(?<= |\t|,|\.|;|"|'|`))(Merge pull request)\s+#(\d+)/gim
+const REPO_INFO = /(github.com\/)([a-zA-Z0-9-_]*)\/([a-zA-Z0-9-_]*)\//gim
 
 Toolkit.run(async (tools) => {
   const bodyList = [];
+  var prId = null;
+  var owner = null;
+  var repo = null;
 
-  const { data: issue } = await tools.github.issues.get({
-    ...tools.context.issue,
-  });
+  // first lets add the commit messages from the context
+  for (let cm of tools.context.payload.commits) {
+    bodyList.push(cm.message)
 
-  if (issue.body) {
-    bodyList.push(issue.body)
-  };
+    // try and get PR id from message
+    var matches = [...cm.message.matchAll(PRID)];
+    for (let item of matches) {
+      if (item.length >= 3 && item[2].length > 0) {
+        tools.log.info(`Found pull request ID in message: ${item[0]}`)
+        prId = item[2];
+      }
+    }
+    // merge context doesnt have easily accessible info so grab the owner/repo
+    if (owner === null && repo === null) {
+      var matches = [...cm.url.matchAll(REPO_INFO)];
+      for (let item of matches) {
+        if (item.length >= 4 && item[2].length > 0 && item[3].length > 0) {
+          owner = item[2];
+          repo = item[3];
+          tools.log.info(`Found repo info: ${owner}/${repo}`)
+        }
+      }      
+    }
+  }
+
+  if (prId !== null && owner !== null && repo !== null) {
+    const { data: issue } = await tools.github.issues.get({
+      owner: owner,
+      repo: repo,
+      issue_number: prId
+    });
   
-  const { data: comments } = await tools.github.issues.listComments({
-    ...tools.context.issue,
-  });
-
-  for (let comment of comments) {
-    bodyList.push(comment.body);
+    if (issue.body) {
+      bodyList.push(issue.body)
+    };
+    
+    const { data: comments } = await tools.github.issues.listComments({
+      owner: owner,
+      repo: repo,
+      issue_number: prId
+    });
+  
+    for (let comment of comments) {
+      bodyList.push(comment.body);
+    }
   }
 
   var issueIds = [];
@@ -30,6 +66,9 @@ Toolkit.run(async (tools) => {
       }
     }
   }
+
+  // unique
+  const unique = [...new Set(issueIds)];
 
   if (issueIds.length <= 0) {
     tools.exit.neutral(
